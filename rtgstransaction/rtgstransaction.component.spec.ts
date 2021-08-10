15808159874dbe773f87f75b5e243b7c057dee6e
sakshi010699 <50827508+sakshi010699@users.noreply.github.com>
2021-08-10 17:32:29 +0530

    Add files via upload

---
+++ rtgstransaction/rtgstransaction.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RTGSTransactionComponent } from './rtgstransaction.component';
+
+describe('RTGSTransactionComponent', () => {
+  let component: RTGSTransactionComponent;
+  let fixture: ComponentFixture<RTGSTransactionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RTGSTransactionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RTGSTransactionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
